a43cb8f0fde8565be58552381a70dcaed576f88a
Naveens08 <60317324+Naveens08@users.noreply.github.com>
2024-07-07 23:55:19 +0530

    Update check_limits.py
    Updated assert to show returned message for invalid parameters.

--- check_limits.py
+++ check_limits.py
@@ -1,19 +1,19 @@
-def temperature_check(temperature):                 #2
+def temperature_check(temperature):                 
     if temperature < 0 or temperature > 45:
         return False, 'Temperature is out of range!'
     return True, ''
     
-def state_of_charge_check(soc):                     #2
+def state_of_charge_check(soc):                     
     if soc < 20 or soc > 80:
         return False, 'State of Charge is out of range!'
     return True, ''
     
-def charge_rate_check(charge_rate):                 #2
+def charge_rate_check(charge_rate):                 
     if charge_rate > 0.8:
         return False, 'Charge rate is out of range!'
     return True, ''
     
-def check_battery_soc(temperature, soc):            #3
+def check_battery_soc(temperature, soc):            
     temp_return_val, temp_return_msg = temperature_check(temperature)
     if not temp_return_val:
         return temp_return_val, temp_return_msg
@@ -24,7 +24,7 @@ def check_battery_soc(temperature, soc):            #3
     
     return True, ''
     
-def battery_is_ok(temperature, soc, charge_rate):   #3
+def battery_is_ok(temperature, soc, charge_rate):   
     return_val, return_msg = check_battery_soc(temperature, soc)
     if not return_val:
         return return_val, return_msg
@@ -37,8 +37,7 @@ def battery_is_ok(temperature, soc, charge_rate):   #3
 
 if __name__ == '__main__':
     return_val, return_msg = battery_is_ok(25, 70, 0.7)
-    assert(return_val is True)
+    assert return_val is True, return_msg
     
     return_val, return_msg = battery_is_ok(50, 85, 0)
-    assert(return_val is False)
-    print(return_msg)
+    assert return_val is True, return_msg
