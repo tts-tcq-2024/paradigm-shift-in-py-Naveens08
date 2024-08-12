5616a8f3c65de1f5487bd9d0a6c6c7540146ee8b
Naveens08 <60317324+Naveens08@users.noreply.github.com>
2024-08-12 12:31:37 +0530

    Create bms_constants.py

---
+++ bms_constants.py
@@ -0,0 +1,21 @@
+TOLERANCE_PERCENTAGE = 0.05
+
+PARAMETER_LIMITS = {
+    "temperature": {"min": 0, "max": 45},
+    "soc": {"min": 20, "max": 80},
+    "charge_rate": {"min": None, "max": 0.8}
+}
+
+WARNING_MESSAGES = {
+    "temperature": {
+        "low": "Warning: Temperature is approaching lower limit!",
+        "high": "Warning: Temperature is approaching upper limit!"
+    },
+    "soc": {
+        "low": "Warning: Approaching discharge!",
+        "high": "Warning: Approaching charge-peak!"
+    },
+    "charge_rate": {
+        "high": "Warning: Charge rate is approaching upper limit!"
+    }
+}
